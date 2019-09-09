2e9d7ba887f5d7f21e7f341187316caa860a1b16
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-09 11:43:11 +0200

    added a printin for encrypting or decrypting a word

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,21 +4,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class App {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Enter a word:");
+        System.out.println("Do You Want To decrypt or encrypt your word?");
+        String want=bufferedReader.readLine();
         String message = "";
         try {
+
+
+            System.out.println("Enter your word:");
             message = bufferedReader.readLine();
             System.out.println("Enter a key value:");
 
             int n= Integer.parseInt(bufferedReader.readLine());
+
+            if(want.equals("decrypt")){
             System.out.println("Text  : " + message);
             System.out.println("Key value : " + n);
+//           System.out.println("Encrypted message: " + Encryptation.encrypt(message, n));
+           System.out.println("Decrypted message: " + Decryptation.decrypt(message, n));}
+            else if(want.equals("encrypt")){
+                System.out.println("Text  : " + message);
+                System.out.println("Key value : " + n);
            System.out.println("Encrypted message: " + Encryptation.encrypt(message, n));
-           System.out.println("Decrypted message: " + Decryptation.decrypt(message, n));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }

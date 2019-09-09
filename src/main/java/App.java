ee6cff8f64177dfa93b749dd9c9891a1e70104c1
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-09 13:03:24 +0200

    added a while loop for the program to continue running and an exit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,33 +5,63 @@ import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Do You Want To decrypt or encrypt your word?");
-        String want=bufferedReader.readLine();
-        String message = "";
-        try {
+        boolean codeRunning = true;
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while (codeRunning) {
+            System.out.println("Do You Want To decrypt or encrypt your word? or you just want to exit?");
+
+            String want = bufferedReader.readLine();
+            String message = "";
+
+            try {
 
 
-            System.out.println("Enter your word:");
-            message = bufferedReader.readLine();
-            System.out.println("Enter a key value:");
 
-            int n= Integer.parseInt(bufferedReader.readLine());
 
-            if(want.equals("decrypt")){
-            System.out.println("Text  : " + message);
-            System.out.println("Key value : " + n);
-           System.out.println("Decrypted message: " + Decryptation.decrypt(message, n));}
-            else if(want.equals("encrypt")){
-                System.out.println("Text  : " + message);
-                System.out.println("Key value : " + n);
-           System.out.println("Encrypted message: " + Encryptation.encrypt(message, n));
+
+
+                if (want.equals("decrypt")) {
+                    System.out.println("Enter your word:");
+                    message = bufferedReader.readLine();
+                    System.out.println("Enter a key value:");
+                    int n = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("Text  : " + message);
+                    System.out.println("Key value : " + n);
+                    System.out.println("Decrypted message: " + Decryptation.decrypt(message, n));
+                } else if (want.equals("encrypt")) {
+                    System.out.println("Enter your word:");
+                    message = bufferedReader.readLine();
+                    System.out.println("Enter a key value:");
+                    int n = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("Text  : " + message);
+                    System.out.println("Key value : " + n);
+                    System.out.println("Encrypted message: " + Encryptation.encrypt(message, n));
+                }
+                else if(want.equals("exit")){
+                    System.out.println("Are you really sure you want to exit?this action is irreversible!type" +
+                            " yes if you want to and no if you made up your mind");
+                    String answer=bufferedReader.readLine();
+                    if(answer.equals("yes")){
+                        System.out.println("Ciao!!!!!!!");
+                        codeRunning = false;
+                    }
+                    else if(answer.equals("no")){
+                        System.out.println("continue enjoying your caesar experience!");
+                        codeRunning=true;
+                    }
+                    else{
+                        System.out.println("Wrong Input!!!!!!");
+                    }
+                }
+                else {
+                    System.out.println("I'm really sorry but we don't recognize your input");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+
         }
-
-
     }
-
 }
